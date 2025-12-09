f7eabf46c7551a243b87554b9c9a1bd7f8a19452
alfabeautymarketing-eng <alfabeautymarketing@gmail.com>
2025-12-09 20:31:11 +0700

    auto: update 20251209-203111

--- server/services/google_service.py
+++ server/services/google_service.py
@@ -11,7 +11,6 @@ SCOPES = [
     'https://www.googleapis.com/auth/script.projects',
     'https://www.googleapis.com/auth/script.external_request',
     'https://www.googleapis.com/auth/script.scriptapp',
-    'https://www.googleapis.com/auth/script.container.ui',
     'https://www.googleapis.com/auth/script.storage',
     'https://www.googleapis.com/auth/documents',
     'https://www.googleapis.com/auth/userinfo.email'
